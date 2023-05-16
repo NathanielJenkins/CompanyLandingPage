631d50e668b2e5fb77f1b41353d2b9d663fe140f
njenkins <nathan.jenkins-boale@skilouise.com>
2023-05-16 11:05:25 -0600

    map swapping

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="pt-24">
-    <div class="container md:px-10 px-4 mx-auto flex flex-wrap flex-col md:flex-row items-center ">
+    <div class="container md:px-10 px-4 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <!--Left Col-->
       <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left ms:order-last">
         <p class="uppercase tracking-loose w-full">Interactive Ski Maps</p>
         <h1 class="my-4 text-5xl font-bold leading-tight">Explore the Slopes Like Never Before!</h1>
-        <p class="leading-normal text-2xl mb-8 text-center md:text-left md:w-auto w-full">Discover Every Trail, Navigate Every Turn.</p>
+        <p class="leading-normal text-2xl mb-8 text-center md:text-left md:w-auto w-full">
+          Discover Every Trail, Navigate Every Turn.
+        </p>
 
         <div class="flex md:w-auto w-full space-x-2 justify-center">
           <!-- make this button a link -->
           <button
-            onclick="window.open('https://skimap.netlify.app/','_blank')"
+            onclick="window.open('https://app.steepdeep.ski/','_blank')"
             class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
           >
             Demo
@@ -19,7 +21,7 @@
       </div>
       <!--Right Col-->
       <div class="w-full md:w-3/5 pb-8 text-center flex flex-col justify-center">
-        <img class=" m-2 w-full md:w-4/5" src="~/assets/img/hero.png" alt />
+        <img class="m-2 w-full md:w-4/5" src="~/assets/img/hero.png" alt />
         <!-- <div class="h-2 w-full md:w-4/5 bg-black rounded-full opacity-20"></div> -->
       </div>
     </div>
--- components/MapSwap.vue
+++ components/MapSwap.vue
@@ -1,19 +1,40 @@
 <template>
-<section class="bg-gray-100 border-b py-8">
-    <div class="container max-w-5xl mx-auto m-8">
+  <section class="bg-gray-100 border-b py-8">
+    <div class="container max-w-5xl mx-auto m-8 text-center">
       <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Swapping Maps</h1>
-      <div class="w-full mb-4">
-        <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-      </div>
-    
+      <span class="text-gray-500"
+        >Seamlessly swap between a real, geo-located, 3D version of your resort and a stylistic marketed world.</span
+      >
+      <client-only placeholder="loading...">
+        <ImgComparisonSlider
+          value="75"
+          tabindex="0"
+          hover="true"
+          class="slider-with-animated-handle rendered mt-8 rounded-lg shadow-lg"
+        >
+          <!-- eslint-disable -->
+          <img slot="first" style="width: 100%" src="~/assets/img/swap-real.min.png" />
+          <img slot="second" style="width: 100%" src="~/assets/img/swap-marketing.min.png" />
+
+          <!-- eslint-enable -->
+        </ImgComparisonSlider>
+      </client-only>
     </div>
   </section>
 </template>
 
-<script>
+<style lang="postcss">
+.slider-with-animated-handle {
+  --default-handle-width: 100px;
+  --default-handle-color: white;
+  stroke-width: 5px;
+}
 
+.slider-with-animated-handle:hover .custom-animated-handle {
+  transform: scale(1.2);
+}
+</style>
 
-export default {
-  name: "MapSwap",
-};
+<script setup lang="ts">
+import { ImgComparisonSlider } from "@img-comparison-slider/vue";
 </script>
