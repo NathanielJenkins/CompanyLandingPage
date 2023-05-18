cc01c30c6cc5b45e2b35d66a1a6684480514cffa
Nathan <nathan.jenkins-boale@constellationhb.com>
2023-05-18 11:57:44 -0600

    change icons

--- components/Features.vue
+++ components/Features.vue
@@ -17,7 +17,7 @@
         Refine your skiing experience with filtering and highlighting. Effortlessly customize your adventure by filtering trails based on based on difficulty and grooming status to find your ideal run. Take it a step further and highlight runs by difficulty or find runs serviceable runs by lifts to easily shape your day. Elevate your ski experience with precision and enjoyment!"
       >
         <div class="flex justify-center">
-          <img src="~/assets/img/destinations.svg" class="flex object-cover h-64" alt />
+          <img src="~/assets/img/filtering.svg" class="flex h-64" alt />
         </div>
       </feature-left>
 
@@ -26,7 +26,7 @@
         content="Share your location with friends easily to easily stay connected while exploring mountain terrain. Say goodbye to confusion and missed connections, and embrace the joy of seamless meetups. Conquer the mountain together!"
       >
       <div class="flex justify-center">
-        <img src="~/assets/img/friends.svg" class="flex object-cover h-64" alt />
+        <img src="~/assets/img/friends.svg" class="flex h-64" alt />
       </div>
       </feature-right>
       <feature-left
@@ -34,14 +34,14 @@
         content="Introducing our interactive ski map Content Management System (CMS) that puts you in control. With our user-friendly interface, internal users can easily access and manage their own content directly on the map. Empower your team with the ability to create and edit map features like: amenities, runs, lifts and areas effortlessly. No need for intermediaries or technical expertise."
       >
         <div class="flex justify-center">
-          <img src="~/assets/img/content.svg" class="flex object-cover h-64" alt />
+          <img src="~/assets/img/content.svg" class="flex h-64" alt />
         </div>
       </feature-left>
 
 
       <feature-right headline="Showcase your Amenities" content="Explore the vast array of mountain amenities at your fingertips. Experience the convenience and effortlessly explore the mountain amenities, such as restaurants and services, enhancing your guests skiing journey."> 
         <div class="flex justify-center">
-          <img src="~/assets/img/amenity.svg" class="flex object-cover h-64" alt />
+          <img src="~/assets/img/amenity.svg" class="flex h-64" alt />
         </div>
       </feature-right>
     
