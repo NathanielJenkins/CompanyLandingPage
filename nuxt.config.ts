5cf0fde12034c7fb4b374d23abda4abecc32c963
Nathan Jenkins <nathan.jenkins-boale@constellationhb.com>
2023-09-30 11:04:49 +0200

    added gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    public: {
+      gtagId: "G-JJB9L26BE5",
     },
   },
 });
