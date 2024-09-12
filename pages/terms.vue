0efbb8499109fa846cacf44559ad433ecfab0fdb
chs-nathan <nathan.jenkins-boale@constellationhb.com>
2024-09-12 14:03:55 -0600

    terms of use

---
+++ pages/terms.vue
@@ -0,0 +1,204 @@
+<template>
+  <svg class="pt-20" viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+        <path
+          d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
+          opacity="0.100000001" />
+        <path
+          d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+          opacity="0.100000001" />
+        <path
+          d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
+          id="Path-4" opacity="0.200000003" />
+      </g>
+      <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+        <path
+          d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z" />
+      </g>
+    </g>
+  </svg>
+  <section class="bg-white border-b text-gray-400 pt-8 pb-8">
+    <div class="flex flex-col items-center justify-center space-x-0 mx-6">
+
+
+
+      <h1>Terms of Use</h1>
+      <p><strong>Effective Date:</strong> October 1st 2022</p>
+
+      <div class="section">
+        <h2>1. Acceptance of Terms</h2>
+        <p>By accessing or using the SteepDeep website (the "Site"), you agree to comply with and be bound by these
+          Terms of Use (the "Terms"). If you do not agree to these Terms, please do not use the Site. We may update
+          these Terms from time to time, and your continued use of the Site constitutes acceptance of any changes.</p>
+      </div>
+
+      <div class="section">
+        <h2>2. Use of the Site</h2>
+        <p>The Site is intended as an exploratory tool for learning about resort facilities, including runs, trails, and
+          amenities. It is not designed to determine the precise location or existance of any geographical features, as
+          the
+          information provided may not be accurate. The Site does not offer
+          real-time or precise geographic data about mountain terrain and should not be relied upon for navigation or
+          assessing the safety of terrain. Information about trail statuses, including open or closed trails, may be
+          inaccurate or outdated. Users must refer to the resort's on-site signage and staff for accurate and
+          current information regarding trail conditions and other geographical features. Guests should refer to
+          on-mountain signage for navigation and trail statuses, regarless of the information posted on the site.</p>
+      </div>
+
+      <div class="section">
+        <h2>3. Accuracy of Information</h2>
+        <p>While we strive to provide accurate and up-to-date information, the Site relies on external data
+          to drive run statuses including difficulty, open-closed and groomed. This data may be inaccurate or
+          out-of-date, and SteepDeep does
+          not take responsibility for any inaccuracies or outdated information. Users should always adhere to the rules
+          and signage of the resort they are on, rather than referring to this digital tool. The Site offers estimates
+          and may not reflect true reality. Actual locations of trails, runs, amenities and all geo-graphical features
+          may vary.</p>
+      </div>
+
+      <div class="section">
+        <h2>4. In-Resort Information and Rules</h2>
+        <p>Guests should always refer to in-resort signage for accurate navigation, difficulty ratings, and
+          information about closures and hazards. The Site is not a substitute for on-resort information. Users must
+          adhere to all posted closures, warnings, and rules at the resort, regardless of what the map indicates. Any
+          changes to the status of trails, runs, or areas at the resort are determined by the resort’s terms of use and
+          not by the Site.</p>
+      </div>
+
+      <div class="section">
+        <h2>5. Dangers and Hazardous Terrain</h2>
+        <p>The Site does not reflect or represent any potential dangers present on the mountain, including but not
+          limited to hazardous terrain, cliffs, or other natural obstacles. These dangers are not shown on the map and
+          are not be accurately depicted. Hazardous terrain may exist on trails or areas outlined on the map, as the
+          locations and conditions depicted (section 7) may not be accurate. The Site is intended for digital
+          exploratory purposes only and
+          should never be used to assess or navigate dangerous terrain. It is the user’s sole responsibility to be aware
+          of and avoid such hazards. Users must exercise caution and adhere to all safety guidelines and warnings
+          provided by the resort to ensure their safety. SteepDeep disclaims any liability for injuries, damages, or
+          losses resulting from the failure to recognize or avoid hazardous conditions, including those that may exist
+          on trails or areas depicted on the Site but not accurately represented.</p>
+      </div>
+
+      <div class="section">
+        <h2>6. Injuries and Liability</h2>
+        <p>SteepDeep does not assume any responsibility for injuries, damages, or losses that may occur while using the
+          resort’s facilities or terrain. The Site is a digital tool designed for informational and exploratory purposes
+          only and should not be relied upon to assess the safety or conditions of the resort. Users are solely
+          responsible for their own safety and must adhere to all resort rules, signage, and warnings. SteepDeep
+          disclaims any liability for injuries, damages, or losses incurred as a result of using the Site or the
+          resort’s facilities. Liability for injuries to life, body, or health due to negligence by SteepDeep or its
+          representatives is strictly limited to cases of deliberate or grossly negligent conduct as defined by
+          applicable laws. Users should exercise caution and consult on-resort staff for the most accurate and current
+          safety information.</p>
+      </div>
+
+      <div class="section">
+        <h2>7. Geographical features</h2>
+        <p>The features depicted on the Site, including but not limited to trails, amenities, services, and other
+          geographical elements, are intended solely for exploratory purposes and are not guaranteed to accurately
+          reflect true reality. These representations may include inaccuracies in terms of location, condition, or
+          existence of geographical features. Users are strongly advised to rely on in-resort information, such as
+          signage and staff guidance, for accurate and current details. SteepDeep assumes no responsibility for any
+          discrepancies or inaccuracies in the Site or in-resort information. Any reliance on the Site for navigation or
+          assessment of geographical features is done at the user’s own risk. Users should verify all relevant
+          information directly with the resort and adhere to any posted guidelines or warnings. SteepDeep disclaims any
+          liability for errors or omissions in the Site or for any consequences resulting from reliance on the
+          information provided herein.</p>
+      </div>
+
+      <div class="section">
+        <h2>7. Trail Status and Conditions</h2>
+        <p>Information regarding trail status, including whether trails are open or closed, groomed or ungroomed, or
+          other conditions, is provided as a guideline only. The Site relies on third-party data which may not be
+          accurate or current. SteepDeep does not guarantee the accuracy of this information. Users should verify trail
+          conditions independently and exercise caution, as conditions can change rapidly due to weather or other
+          factors.</p>
+      </div>
+
+      <div class="section">
+        <h2>8. No Warranty</h2>
+        <p>The Site is provided on an "as-is" basis without any warranties of any kind, either express or implied.
+          SteepDeep does not warrant that the Site will be uninterrupted, error-free, or free of viruses or other
+          harmful components. Users are responsible for any consequences of using or relying on the Site, including any
+          potential damage to their device or data.</p>
+      </div>
+
+      <div class="section">
+        <h2>9. Limitation of Liability</h2>
+        <p>SteepDeep is not liable for any damages or losses incurred as a result of using the Site, including but not
+          limited to direct, indirect, incidental, or consequential damages. This includes any loss of data,
+          interruption of service, or any other issues that may arise. Liability for injuries to life, body, or health
+          due to negligence by SteepDeep or its representatives is limited to legally defined deliberate or negligent
+          conduct.</p>
+      </div>
+
+      <div class="section">
+        <h2>10. Intellectual Property</h2>
+        <p>All content on the Site, excluding resort uploaded content, including but not limited to text, graphics,
+          logos, and software, is the property of
+          SteepDeep or its licensors and is protected by copyright, trademark, and other intellectual property laws.
+          Unauthorized use of any content from the Site is prohibited.</p>
+      </div>
+
+      <div class="section">
+        <h2>11. User Conduct</h2>
+        <p>You agree to use the Site in accordance with all applicable laws and regulations. You may not use the Site
+          for any unlawful purpose or in a way that could damage, disable, overburden, or impair the Site or interfere
+          with any other party’s use and enjoyment of the Site.</p>
+      </div>
+
+      <div class="section">
+        <h2>12. Third-Party Links</h2>
+        <p>The Site may contain links to third-party websites or resources. SteepDeep is not responsible for the
+          availability or accuracy of such external sites or resources and does not endorse and is not responsible for
+          any content, products, or services available from such sites or resources.</p>
+      </div>
+
+      <div class="section">
+        <h2>13. Indemnification</h2>
+        <p>You agree to indemnify, defend, and hold harmless SteepDeep, its affiliates, officers, directors, employees,
+          and agents from and against any claims, liabilities, damages, losses, and expenses (including reasonable
+          attorneys’ fees) arising out of or related to your use of the Site or any violation of these Terms.</p>
+      </div>
+
+      <div class="section">
+        <h2>14. Governing Law</h2>
+        <p>These Terms shall be governed by and construed in accordance with the laws of the Province of British
+          Columbia, Canada, without regard to its conflict of law principles. Any legal action or proceeding arising
+          under or relating to these Terms shall be brought exclusively in the courts located in British Columbia.</p>
+      </div>
+
+      <div class="section">
+        <h2>15. Termination</h2>
+        <p>SteepDeep reserves the right to terminate or suspend your access to the Site at any time, without notice, for
+          any reason, including if we believe you have violated these Terms.</p>
+      </div>
+
+
+    </div>
+
+  </section>
+</template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: "SteepDeep | Terms of Use",
+  description: "Terms of use of the product",
+});
+</script>
+
+<style lang="postcss" scoped>
+h1 {
+  @apply text-xl;
+}
+
+h2 {
+  @apply font-bold;
+}
+
+.section {
+  @apply mb-4;
+}
+</style>
